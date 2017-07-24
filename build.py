5b58e9beea9851082d8f26cb451ea0ec1ffb264e
vtpl1 <monotosh.das@videonetics.com>
2017-07-24 08:20:06 +0530

    Update build.py

--- build.py
+++ build.py
@@ -2,7 +2,6 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="vtpl1", channel="stable",
-                                 upload="https://api.bintray.com/conan/vtpl1/conan-expat")
+    builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="Expat:shared")
     builder.run()
